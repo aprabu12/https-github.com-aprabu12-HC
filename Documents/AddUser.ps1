e2feeb0fc6372d534e7853f1633a8ee4858db5e1
Anu Prabu <Anuprabu@yahoo.com>
2019-10-01 12:49:01 -0400

    Added Powershell scrips and APi documenttaion to the document folder

---
+++ Documents/AddUser.ps1
@@ -0,0 +1,25 @@
+﻿$params = '
+{
+  "userID": 14,
+  "firstName": "dee",
+  "lastName": "alston",
+  "age": 32,
+  "phone": "1231231234",
+  "email": "",
+  "hobbies": "",
+  "isActive": true,
+  "addressID": 2,
+  "address": {
+    "addressID": 2,
+    "address1": "100 cumming drive",
+    "address2": "",
+    "city": "cumming",
+    "state": "GA",
+    "zip": "30000",
+    "country": "USA"
+  }
+  }'
+
+
+
+Invoke-RestMethod -Uri https://localhost:44340/api/users -Method POST -Body $params -ContentType "application/json"
---
+++ Documents/SearchUsers.ps1
@@ -0,0 +1,11 @@
+﻿ Invoke-RestMethod -Uri https://localhost:44340/api/users -Method Get;
+ $data =Invoke-RestMethod -Uri https://localhost:44340/api/users -Method Get;
+ $data.Count
+ $data.Where({$_.userID},'Default',13)
+ $data.Where({$_.lastName -eq 'jones'})
+
+
+
+
+
+
